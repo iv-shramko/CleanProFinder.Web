cbf65bb2a6d9045018456727b1d65717a5f0f27c
grimerssy <grimerssy@gmail.com>
2023-10-31 09:14:51 +0100

    add card design shared component

---
+++ CleanProFinder.Web/src/app/modules/shared/components/card-design/card-design.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'card-design',
+  templateUrl: './card-design.component.html',
+  styleUrls: ['./card-design.component.scss'],
+})
+export class CardDesignComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
